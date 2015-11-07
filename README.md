7bed4cc1bbe6d3655c505a7ec6cd2e8efa21b50a
Gianluca <gl051@biccari.com>
2015-11-06 18:51:14 -0800

    Refactoring and added a config file

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 A simple application for scraping public instagram accounts and download the most recent pictures.
 
-This software has been written as en exercise for me to get familiar with the following Python modules:
+This software has been written as en exercise to get familiar with the following Python modules:
 
 - Request
 - Beautiful Soup
 - Threading
 
-Anyone using this software will be responsable for the use it makes of the pictures downloaded on its own computer, I suggest to use this application only as a tool to download and backup your own instagram photos.
+Anyone using this software will be responsible for the use it makes of the pictures downloaded on its own computer, I suggest to use this application only as a tool to download and backup your own instagram photos.
 
 To run the program:
 
@@ -16,4 +16,3 @@ To run the program:
 2. Call the program with the following command:
    python instagram_scraper.py
 3. Multiple threads, one per each instagram account, will be started to download the most recent pictures, duplicated are skipped. Under the folder 'instagram_downloads' you will find the photos organized by instagram handler.
-
--- instagram_scraper.py
+++ instagram_scraper.py
@@ -1,31 +1,43 @@
+#!/usr/bin/python
+import sys
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
 import os
 import threading
 
-# List here the instagram accounts you want to download
-urllist = (
-'https://instagram.com/taylorswift/',
-'http://instagram.com/usainbolt',
-'https://instagram.com/neymarjr/',
-'https://instagram.com/burberry',
-'https://instagram.com/vevo/'
-)
+verbose_mode = False;
 
-download_folder = os.path.join(os.getcwd(), 'instagram_downloads')
 
-def downloadUrl(url):
+def get_urls(config_file):
+    if not os.path.isfile(config_file):
+        print "Can't find file {}".format(config_file)
+        print "Be sure to create a config file as the one provided as a sample"
+        return None
+    ret_list = []
+    with open(config_file,'r') as fin:
+        for line in fin:
+            ret_list.append(line.strip('\n'))
+    return ret_list
+
+
+def download_url(url):
+    global verbode_mode
+    # Create directory for downloads if not exists already
+    download_folder = os.path.join(os.getcwd(), 'instagram_downloads')
+    if not os.path.exists(download_folder):
+        os.mkdir(download_folder)
+
     res = requests.get(url)
     # Raise expection if we couldn't read the webpage
     res.raise_for_status()
-
     # Folder name
     instagram_handle = url[url.find('instagram.com')+len('instagram.com')+1:-1]
     folder_path = os.path.join(download_folder, instagram_handle)
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
 
+    print 'Started download {}'.format(instagram_handle)
     soup = BeautifulSoup(res.text,"html.parser")
     # All the pictures URLs to download are in the script tag
     scripts = soup.select('script')
@@ -35,7 +47,8 @@ def downloadUrl(url):
     # to detect the start of each picture URL. The first element
     # of the slipt is not a picture.
     nrange = max(0,len(picscript.text.split('"display_src":'))-1)
-    for i in range(1, nrange):
+    counter = 0
+    for i in xrange(1, nrange):
         urlpic = picscript.text.split('"display_src":')[i].split('},')[0]
         # "https:\\/\\/scontent.cdninstagram.com\\/hphotos-xfa1\\/t51.2885-15\\/e15\\/11809535_437579513099534_959907557_n.jpg"
         urlpic = urlpic.replace("\\","")
@@ -45,29 +58,41 @@ def downloadUrl(url):
         if not os.path.exists(path):
             r = requests.get(urlpic)
             if r.status_code == 200:
-    		    with open(path, 'wb') as f:
+                with open(path, 'wb') as f:
     			    for chunk in r.iter_content(1024):
     				    f.write(chunk)
-            print 'Downloaded picture ', path
-
-print "Start application"
-# List of download threads, to speed up the process.
-downloadThreads = []
-# Create directory for downloads if not exists
-if not os.path.exists(download_folder):
-    os.mkdir(download_folder)
-
-for myurl in urllist:
-    print 'Start download for {}'.format(myurl)
-    downloader = threading.Thread(target=downloadUrl, args=(myurl,))
-    downloadThreads.append(downloader)
-    downloader.start()
-
-# Wait for all downloaders to complete
-for downloader in downloadThreads:
-    downloader.join()
-
-print 'All downloaded completed'
+                counter += 1
+            if verbose_mode:
+                print 'Downloaded picture ', path
+    print 'Completed download {} with {} new pictures'.format(instagram_handle, counter)
 
 
+# Gather our code in a main() function
+def main():
+    global verbose_mode
 
+    # Check if verbose mode is on
+    if len(sys.argv) > 1:
+        if sys.argv[1].lower() == '-verbose':
+            verbose_mode = True
+
+    # List of download threads, to speed up the process.
+    downloadThreads = []
+
+    # Get the instagram URLs from the config file
+    urls = get_urls("config.txt")
+
+    # Per each url start a downaload thread
+    for url in urls:
+        downloader = threading.Thread(target=download_url, args=(url,))
+        downloadThreads.append(downloader)
+        downloader.start()
+
+    #Wait for all downloaders to complete
+    for downloader in downloadThreads:
+        downloader.join()
+    print 'All done.'
+    
+# Standard boilerplate to call the main() function to begin the program.
+if __name__ == '__main__':
+  main()
